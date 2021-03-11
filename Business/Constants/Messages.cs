c90fd2eceda814cf424ed28dbf25326c614538bd
CustoMOyuncU <kraloyuntv2@gmail.com>
2021-03-11 22:04:57 +0300

    Son BackEnd Dersi

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,6 +10,8 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Ürün eklendi";
+        public static string ProductUpdated = "Ürün güncellendi";
+        public static string ProductDeleted = "Ürün silindi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string ProductListed = "Ürünler listelendi";
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
@@ -18,13 +20,14 @@ namespace Business.Constants
         public static string MaintenanceTime = "Sistem bakımda";
         public static string CategoryLimitExceded = "Kategori limiti aşıldı";
         public static string AuthorizationDenied = "Yetkiniz yok";
+
         public static string UserRegistered = "Kayıt oldu";
         public static string UserNotFound = "Kullanıcı bulunamadı";
-        public static string PasswordError="Şifre yanlış";
-        public static string SuccessfulLogin="Giriş başarılı";
-        public static string UserAlreadyExists="Kullanıcı mebcut";
-        public static string AccessTokenCreated="jwt Created";
-        internal static string ProductUpdated;
-        internal static string ProductDeleted;
+
+        public static string PasswordError = "Şifre yanlış";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı mebcut";
+        public static string AccessTokenCreated = "jwt Created";
+        
     }
 }
